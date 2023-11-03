40c38b12de5d8ab54ac9f6f4f34047d0cbe84801
Michaela Bačíková <bacikova.michaela@gmail.com>
2023-11-03 14:59:47 +0100

    priklad matice v ArrayOfArrays

--- Arrays/ArrayOfArrays.java
+++ Arrays/ArrayOfArrays.java
@@ -2,19 +2,78 @@ public class ArrayOfArrays {
     public static void main(String[] args) {
         //int[][] aMatrix = new int[4][];
         int[][] aMatrix = new int[4][5];
+//        printMatrix(aMatrix);
 
-        //populate matrix
-        for (int i = 0; i < aMatrix.length; i++) {
-            //aMatrix[i] = new int[5];	//create sub-array
-            for (int j = 0; j < aMatrix[i].length; j++) {
-                aMatrix[i][j] = i + j;
+        int[][] a = {
+                { 1, 2, 3, 4 },
+                { 6, 5, 4, 19 },
+                { 7, 8, 9, 10 }
+        };
+        int[][] b = {
+                { 4, 6, 8, 8 },
+                { 5, 7, 9, 10 },
+                { 10, 12, 50, 20 }
+        };
+
+        int[][] result = addMatrices(a, b);
+        if(result != null) {
+            printMatrix(result);
+        }
+
+        int[][] matrixToTranspose = {
+                { 0, 1, 2, 3 },
+                { 4, 5, 6, 7 }
+        };
+        int[][] transposedMatrix = transposeMatrix(matrixToTranspose);
+        printMatrix(transposedMatrix);
+
+//        //populate matrix
+//        for (int r = 0; r < aMatrix.length; r++) {
+//            //aMatrix[i] = new int[5];	//create sub-array
+//            for (int c = 0; c < aMatrix[r].length; c++) {
+//                aMatrix[r][c] = r + c;
+//            }
+//        }
+//
+//        //print matrix
+//        for (int r = 0; r < aMatrix.length; r++) {
+//            for (int c = 0; c < aMatrix[r].length; c++) {
+//                System.out.print(aMatrix[r][c] + " ");
+//            }
+//            System.out.println();
+//        }
+    }
+
+    private static int[][] addMatrices(int[][] a, int[][] b) {
+        if(a.length != b.length) {
+            System.out.println("NEDA SA ZRATAT, MATICE MUSIA MAT ROVNAKY POCET RIADKOV");
+            return null;
+        }
+
+        int[][] result = new int[3][4]; //vysledok
+
+        for (int r = 0; r < a.length; r++) {
+            for (int c = 0; c < a[r].length; c++) {
+                result[r][c] = a[r][c] + b[r][c];
             }
         }
 
-        //print matrix
-        for (int i = 0; i < aMatrix.length; i++) {
-            for (int j = 0; j < aMatrix[i].length; j++) {
-                System.out.print(aMatrix[i][j] + " ");
+        return result;
+    }
+
+    private static int[][] transposeMatrix(int[][] m) {
+        int[][] result = null;// = new int[][];
+
+        //transponovat maticu
+        //https://cs.wikipedia.org/wiki/Transpozice_matice
+
+         return result;
+    }
+
+    private static void printMatrix(int[][] aMatrix) {
+        for (int r = 0; r < aMatrix.length; r++) {
+            for (int c = 0; c < aMatrix[r].length; c++) {
+                System.out.print(aMatrix[r][c] + " ");
             }
             System.out.println();
         }
